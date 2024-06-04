2fce76470a9f05cf8d23cf41594bce9d34ec46c8
davide laneri <davidelan@hotmail.com>
2024-06-04 13:47:28 +0000

    Correct sendEMail.js function

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,5 +1,5 @@
 function sendMail(contactForm) {
-    emailjs.send("otlook", "davide", {
+    emailjs.send("outlook", "davide", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "project_request": contactForm.projectsummary.value
